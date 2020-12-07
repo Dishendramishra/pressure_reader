c594b2c9130ab44d4993afa738e3c19ffabed3bc
unknown <dishendramishra@gmail.com>
2020-12-07 16:00:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Controller Used	
+
+**XGS-600 Vacuum Gauge Controller** 
+
+<img src="C:\Users\ryzen5\Desktop\pressure_reader\xgs-600.jpg" style="zoom:50%;" />
+
+We had Model No. XGS600H0M0C0A1
+
+
+
+## Requirements
+
+```shell
+pip install -r requirements
+```
+
---
+++ main.py
@@ -0,0 +1,72 @@
+import serial
+import serial.tools.list_ports
+from time import sleep
+
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+import csv
+
+from datetime import datetime
+
+# =====================================
+#           vars plotting data
+# =====================================
+matplotlib.use('TkAgg')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.show()
+i = 0
+x, y = [], []
+# =====================================
+
+# =====================================================
+#           serial communication
+# =====================================================
+# comment line 25-27 for identifying your hw id
+# then comment line 24 and uncomment 25-27
+ports = serial.tools.list_ports.comports()
+target_port = None
+for port, desc, hwid in sorted(ports):
+    # print("{}: {} [{}]".format(port, desc, hwid))
+    if "PID=1A86:7523" in hwid:
+        print(port)
+        target_port = port
+# =====================================================
+
+ser = serial.Serial(target_port, 9600)
+file = open("data.csv","w",newline='')
+file_writer = csv.writer(file)
+
+while True:
+    try:
+        now = datetime.now()    # current datetime
+
+        ser.write(b'#000F')     # cmd to read data
+        ser.flush()
+        sleep(0.1)
+        line = ser.read(21).decode()
+
+        # aux1= float(line[:line.find(",")-1])  # extracting aux1 value
+        aux2 = float(line[line.find(",")+1:]) # extracting aux2 value
+        print(aux2)
+        
+        x.append(i)
+        y.append(aux2)
+        ax.plot(x, y, color='b')
+        fig.canvas.draw()
+        ax.set_xlim(left=max(0, i-50), right=i+50)
+        i += 1
+
+        date = now.strftime("%d-%m-%Y")
+        time = now.strftime("%H:%M:%S")
+
+        file_writer.writerow([aux2, time, date])
+        sleep(0.9)
+
+    except:
+        file.flush()
+        file.close()
+
+# limits
+# y: (mbar) 10**-6 to 1400
